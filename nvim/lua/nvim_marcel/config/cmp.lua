cd131530890dd74eb3c2de632a369f02a96cd20d
Marcel Beumer <marcel@marcelbeumer.com>
2021-10-01 23:39:48 +0200

    ...

--- nvim/lua/nvim_marcel/config/cmp.lua
+++ nvim/lua/nvim_marcel/config/cmp.lua
@@ -34,9 +34,9 @@ cmp.setup({
     comparators = {
       cmp.config.compare.offset,
       cmp.config.compare.exact,
-      cmp.config.compare.score,
       -- cmp.config.compare.kind,
       cmp.config.compare.sort_text,
+      cmp.config.compare.score,
       cmp.config.compare.length,
       cmp.config.compare.order,
 
