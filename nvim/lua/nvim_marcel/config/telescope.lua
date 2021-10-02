d69bd61b8a3e4b316bfbb51b17c5eb2afc9e523a
Marcel Beumer <marcel@marcelbeumer.com>
2021-10-02 02:10:39 +0200

    ...

--- nvim/lua/nvim_marcel/config/telescope.lua
+++ nvim/lua/nvim_marcel/config/telescope.lua
@@ -2,27 +2,17 @@ local action_set = require("telescope.actions.set")
 
 require("telescope").setup({
   defaults = {
-    file_sorter = require("telescope.sorters").get_fzy_sorter,
-    generic_sorter = require("telescope.sorters").get_fzy_sorter,
     mappings = {
       i = {
         ["<C-q>"] = require("telescope.actions").send_to_qflist,
       },
     },
   },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = false,
-      override_file_sorter = true,
-      case_mode = "smart_case",
-    },
-  },
   pickers = {
     find_files = {
       hidden = true,
-      --- workaround for folds not working when opening a file
-      --- https://github.com/nvim-telescope/telescope.nvim/issues/559
+      -- workaround for folds not working when opening a file
+      -- https://github.com/nvim-telescope/telescope.nvim/issues/559
       attach_mappings = function()
         action_set.select:enhance({
           post = function()
@@ -34,6 +24,9 @@ require("telescope").setup({
     },
   },
 })
+
+-- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+require("telescope").load_extension("fzf")
 
 local function split_str(v)
   local args = {}
