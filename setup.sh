556d2b80e2d53f13c946bb157eea6de7ba9bd900
Marcel Beumer <marcel@marcelbeumer.com>
2022-03-27 11:32:41 +0200

    Remove vim config

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 dirname=`pwd`/`dirname "$0"`
 
-rm -f ~/.vimrc && ln -s $dirname/vimrc ~/.vimrc
 rm -f ~/.editorconfig && ln -s $dirname/editorconfig ~/.editorconfig
 rm -f ~/.gitconfig && ln -s $dirname/gitconfig ~/.gitconfig
 rm -f ~/.zshrc && ln -s $dirname/zshrc ~/.zshrc
@@ -13,10 +12,3 @@ rm -rf ~/.config/nvim && ln -s $dirname/nvim ~/.config/nvim
 rm -rf ~/.config/kitty && ln -s $dirname/kitty ~/.config/kitty
 rm -rf ~/.config/alacritty && ln -s $dirname/alacritty ~/.config/alacritty
 rm -rf ~/.config/karabiner && ln -s $dirname/karabiner ~/.config/karabiner
-
-mkdir -p ~/.vim
-mkdir -p ~/.vim/swp-vimrc-coc
-mkdir -p ~/.vim/swp-vimrc-vim-lsp
-rm -f ~/.vimrc && ln -s $dirname/vim/vimrc ~/.vimrc
-rm -f ~/.vim/config && ln -s $dirname/vim/config ~/.vim/config
-rm -f ~/.vim/coc-settings.json && ln -s $dirname/vim/coc-settings.json ~/.vim/coc-settings.json
