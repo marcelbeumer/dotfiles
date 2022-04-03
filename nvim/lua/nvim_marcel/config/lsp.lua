3b457b151b6296b6972137f4bd30e00a401233c1
Marcel Beumer <marcel@marcelbeumer.com>
2022-04-03 20:03:18 +0200

    Use golines for formatting

--- nvim/lua/nvim_marcel/config/lsp.lua
+++ nvim/lua/nvim_marcel/config/lsp.lua
@@ -65,7 +65,8 @@ local function setup_null_ls()
   local sources = {
     null_ls.builtins.formatting.stylua,
     -- null_ls.builtins.formatting.gofmt,
-    null_ls.builtins.formatting.goimports,
+    -- null_ls.builtins.formatting.goimports,
+    null_ls.builtins.formatting.golines,
     null_ls.builtins.diagnostics.golangci_lint,
     null_ls.builtins.formatting.black,
     -- null_ls.builtins.diagnostics.pylint,
