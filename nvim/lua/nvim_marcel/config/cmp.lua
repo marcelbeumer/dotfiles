2a50fd88c7637f49afb2cc63f8e93453d438dbf4
Marcel Beumer <marcel@marcelbeumer.com>
2022-04-14 21:14:24 +0200

    fix next/prev behavior for cmp

--- nvim/lua/nvim_marcel/config/cmp.lua
+++ nvim/lua/nvim_marcel/config/cmp.lua
@@ -53,8 +53,8 @@ cmp.setup({
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<down>"] = cmp.mapping.select_next_item(),
-    ["<up>"] = cmp.mapping.select_prev_item(),
+    ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+    ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
     ["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
   },
   formatting = {
