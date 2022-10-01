a6fd45761a35786a20a2829dfcdfc69a9c55f37e
Marcel Beumer <marcel@marcelbeumer.com>
2022-10-01 12:02:26 +0200

    add ideavimrc

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ dirname=`pwd`/`dirname "$0"`
 rm -f ~/.editorconfig && ln -s $dirname/editorconfig ~/.editorconfig
 rm -f ~/.gitconfig && ln -s $dirname/gitconfig ~/.gitconfig
 rm -f ~/.zshrc && ln -s $dirname/zshrc ~/.zshrc
+rm -f ~/.ideavimrc && ln -s $dirname/ideavimrc ~/.ideavimrc
 
 mkdir -p ~/.config
 
