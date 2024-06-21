d8a2e243f34daef1a314f38efa824cb8c9d58a6e
Marcel Beumer <marcel@marcelbeumer.com>
2024-06-21 18:53:42 +0200

    feat: tweak linux terminals

--- linux/setup.sh
+++ linux/setup.sh
@@ -8,3 +8,8 @@ rm -f ~/.stylua && ln -s $dirname/stylua ~/.stylua
 rm -f ~/.editorconfig && ln -s $dirname/editorconfig ~/.editorconfig
 rm -f ~/.gitconfig && ln -s $dirname/gitconfig ~/.gitconfig
 rm -f ~/.gitignore && ln -s $dirname/gitignore ~/.gitignore
+
+mkdir -p ~/.config
+
+rm -rf ~/.config/kitty && ln -s $dirname/kitty ~/.config/kitty
+rm -rf ~/.config/alacritty && ln -s $dirname/alacritty ~/.config/alacritty
