686b31ac0a778ed3e1701446934cc73e367bf70e
Marcel Beumer <marcel@marcelbeumer.com>
2024-06-25 23:35:19 +0200

    feat: tmux

--- linux/setup.sh
+++ linux/setup.sh
@@ -4,6 +4,7 @@ dirname=`pwd`/`dirname "$0"`
 
 rm -f ~/.bashrc && ln -s $dirname/bashrc ~/.bashrc
 rm -f ~/.bash_profile && ln -s $dirname/bash_profile ~/.bash_profile
+rm -f ~/.tmux.conf && ln -s $dirname/tmux.conf ~/.tmux.conf
 rm -f ~/.stylua && ln -s $dirname/stylua ~/.stylua
 rm -f ~/.editorconfig && ln -s $dirname/editorconfig ~/.editorconfig
 rm -f ~/.gitconfig && ln -s $dirname/gitconfig ~/.gitconfig
